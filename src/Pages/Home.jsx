afc403dc8a0caefddd007f9923c2e63b85290504
kawsar12759 <kawsar15-12759@diu.edu.bd>
2025-05-17 13:37:15 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/Logo.png'
+
+const Home = () => {
+    return (
+        <div className=''>
+            <div className='flex justify-center py-[58px]'><img className='h-12' src={logo} alt="" /></div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Home from './Pages/Home';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
