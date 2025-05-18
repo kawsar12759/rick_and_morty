629b3801778b04f45934b66e2444eb88aa64dbc1
kawsar12759 <kawsar15-12759@diu.edu.bd>
2025-05-18 22:23:47 +0600

    Worked on Episodes and Locations

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,11 @@ import { IoIosArrowBack } from "react-icons/io";
 
 const Home = () => {
     const [characters, setCharacters] = useState([]);
-    const scrollRef = useRef();
+    const [episodes, setEpisodes] = useState([]);
+    const [locations, setLocations] = useState([]);
+    const characterRef = useRef();
+    const episodeRef = useRef();
+    const locationRef = useRef();
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
@@ -20,9 +24,28 @@ const Home = () => {
             })
             .catch(err => console.error(err));
     }, []);
-    const scroll = (direction) => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollBy({
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/episode')
+            .then(res => res.json())
+            .then(data => {
+                setEpisodes(data.results);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/location')
+            .then(res => res.json())
+            .then(data => {
+                setLocations(data.results);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
+    const handleScroll = (ref, direction) => {
+        if (ref.current) {
+            ref.current.scrollBy({
                 left: direction === 'left' ? -200 : 200,
                 behavior: 'smooth',
             });
@@ -52,17 +75,17 @@ const Home = () => {
                         in other worlds and alternate dimensions.</p>
                 </div>
             </div>
-            <div className="relative w-full  mx-auto px-16 py-8">
+            <div className="relative w-full  mx-auto px-[102px] pt-8">
                 <div className="flex justify-between items-center mb-3">
-                    <h2 style={{ fontFamily: 'TTTravels-Medium' }} className="text-white text-2xl font-semibold">Meet The Cast</h2>
-                    <button className="text-white bg-transparent border border-[#9DFE00] h-10 w-28 rounded-lg px-3 py-1  hover:bg-white hover:text-black transition">
+                    <h2 style={{ fontFamily: 'TTTravels-Medium' }} className="text-white text-2xl font-medium">Meet The Cast</h2>
+                    <button className="text-white bg-transparent border border-[#9DFE00] h-10 w-28 rounded-lg px-3 py-1  hover:bg-[#9DFE00]  transition">
                         View All
                     </button>
                 </div>
 
                 <div className="relative">
                     <div
-                        ref={scrollRef}
+                        ref={characterRef}
                         className="flex overflow-y-hidden overflow-x-auto gap-8 scroll-smooth scrollbar-hide pb-4"
                     >
                         {characters.map((char) => (
@@ -70,7 +93,7 @@ const Home = () => {
                                 key={char.id}
                                 style={{ clipPath: 'polygon(0 0, 100% 0, 100% 82%, 82% 100%, 0 100%)' }}
                                 className="min-w-[290px] h-[296px] p-[1px]  bg-gradient-to-r from-[#84F729] to-[#15bffd]  rounded-xl">
-                                <div style={{ clipPath: 'polygon(0 0, 100% 0, 100% 82%, 82% 100%, 0 100%)' }} className='bg-black h-full w-full rounded-xl text-white  p-4  group'>
+                                <div style={{ clipPath: 'polygon(0 0, 100% 0, 100% 82%, 82% 100%, 0 100%)' }} className='bg-[#262934] h-full w-full rounded-xl text-white  p-4  group'>
                                     <img
                                         src={char.image}
                                         alt={char.name}
@@ -86,14 +109,99 @@ const Home = () => {
                     </div>
 
                     <button
-                        onClick={() => scroll('right')}
+                        onClick={() => handleScroll(characterRef, 'right')}
                         className="absolute -right-6 top-1/2 transform -translate-y-1/2 z-10 bg-white w-12 h-12 rounded-full shadow-md flex items-center text-2xl text-[#9DFE00] font-bold justify-center  hover:bg-gray-300 transition"
                     >
                         <IoIosArrowForward />
                     </button>
 
                     <button
-                        onClick={() => scroll('left')}
+                        onClick={() => handleScroll(characterRef, 'left')}
+                        className="absolute -left-6 top-1/2 transform -translate-y-1/2 z-10 bg-white text-[#9DFE00] w-12 h-12 rounded-full text-2xl font-bold shadow-md flex items-center justify-center hover:bg-gray-300 transition"
+                    >
+                        <IoIosArrowBack />
+                    </button>
+                </div>
+            </div>
+
+            <div className="relative w-full  mx-auto px-[102px] py-28">
+                <div className="flex justify-between items-center mb-8">
+                    <h2 style={{ fontFamily: 'TTTravels-Medium' }} className="text-white text-2xl font-medium">Episodes</h2>
+                </div>
+
+                <div className="relative">
+                    <div
+                        ref={episodeRef}
+                        className="flex overflow-y-hidden overflow-x-auto gap-8 scroll-smooth scrollbar-hide pb-4"
+                    >
+                        {episodes.map((episode) => (
+                            <div
+                                key={episode.id}
+                                style={{ clipPath: 'polygon(0 0, 100% 0, 100% 60%, 87% 100%, 0 100%)' }}
+                                className="min-w-[360px] h-[96px] p-[1px]  bg-gradient-to-r from-[#84F729] to-[#15bffd]  rounded-xl">
+                                <div style={{ clipPath: 'polygon(0 0, 100% 0, 100% 60%, 87% 100%, 0 100%)' }} className='bg-[#262934] h-full w-full rounded-xl text-white  flex items-center p-6  group'>
+                                    <div style={{ fontFamily: 'TTTravels-Medium' }}>
+                                        <h4 className='text-lg font-light'>{episode.episode}</h4>
+                                        <p className=" text-2xl font-medium">{episode.name}</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+
+                    <button
+                        onClick={() => handleScroll(episodeRef, 'right')}
+                        className="absolute -right-6 top-1/2 transform -translate-y-1/2 z-10 bg-white w-12 h-12 rounded-full shadow-md flex items-center text-2xl text-[#9DFE00] font-bold justify-center  hover:bg-gray-300 transition"
+                    >
+                        <IoIosArrowForward />
+                    </button>
+
+                    <button
+                        onClick={() => handleScroll(episodeRef, 'left')}
+                        className="absolute -left-6 top-1/2 transform -translate-y-1/2 z-10 bg-white text-[#9DFE00] w-12 h-12 rounded-full text-2xl font-bold shadow-md flex items-center justify-center hover:bg-gray-300 transition"
+                    >
+                        <IoIosArrowBack />
+                    </button>
+                </div>
+            </div>
+
+
+            <div className="relative w-full  mx-auto px-[102px] pb-20">
+                <div className="flex justify-between items-center mb-8">
+                    <h2 style={{ fontFamily: 'TTTravels-Medium' }} className="text-white text-2xl font-medium">Locations</h2>
+                </div>
+
+                <div className="relative">
+                    <div
+                        ref={locationRef}
+                        className="flex overflow-y-hidden overflow-x-auto gap-8 scroll-smooth scrollbar-hide pb-4"
+                    >
+                        {locations.map((location) => (
+                            <div
+                                key={location.id}
+                                style={{ clipPath: 'polygon(0 0, 100% 0, 100% 60%, 87% 100%, 0 100%)' }}
+                                className="min-w-[360px] h-[96px] p-[1px]  bg-gradient-to-r from-[#84F729] to-[#15bffd]  rounded-xl">
+                                <div style={{ clipPath: 'polygon(0 0, 100% 0, 100% 60%, 87% 100%, 0 100%)' }} className='bg-[#262934] h-full w-full rounded-xl text-white  flex items-center p-6  group'>
+                                    <div style={{ fontFamily: 'TTTravels-Medium' }}>
+                                        <h4 className='text-lg font-light'>#{location.id}</h4>
+                                        <p className=" text-2xl font-medium">{location.name}</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+
+                    <button
+                        onClick={() => handleScroll(locationRef, 'right')}
+                        className="absolute -right-6 top-1/2 transform -translate-y-1/2 z-10 bg-white w-12 h-12 rounded-full shadow-md flex items-center text-2xl text-[#9DFE00] font-bold justify-center  hover:bg-gray-300 transition"
+                    >
+                        <IoIosArrowForward />
+                    </button>
+
+                    <button
+                        onClick={() => handleScroll(locationRef, 'left')}
                         className="absolute -left-6 top-1/2 transform -translate-y-1/2 z-10 bg-white text-[#9DFE00] w-12 h-12 rounded-full text-2xl font-bold shadow-md flex items-center justify-center hover:bg-gray-300 transition"
                     >
                         <IoIosArrowBack />
